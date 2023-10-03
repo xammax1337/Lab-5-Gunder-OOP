112eecc919256e4cb57454871de8451ace3a8a24
ordio <max@maxsamuelsson.se>
2023-10-03 15:30:55 +0200

    Add project files.

---
+++ Lab 5 Gunder OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab_5_Gunder_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            double area1 = circle1.GetArea();
+
+            Circle circle2 = new Circle(6);
+            double area2 = circle2.GetArea();
+
+            Console.WriteLine("Circle with Radius 5 has an area of: " + area1);
+            Console.WriteLine("Circle with Radius 6 has an area of: " + area2);
+        }
+    }
+
+    class Circle
+    {
+        private int radius;
+        private const double pi = Math.PI;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public double GetArea()
+        {
+            return radius * radius * pi;
+        }
+    }
+}
